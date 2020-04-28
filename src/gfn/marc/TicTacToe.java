2a81a0342e7975b2b9268d1e464e7d1dc833c7af
Marc Michalsky <m.michalsky@posteo.de>
2020-04-28 22:32:16 +0200

    Spiel wird als eigener Thread gestartet

--- src/gfn/marc/TicTacToe.java
+++ src/gfn/marc/TicTacToe.java
@@ -9,30 +9,38 @@ public class TicTacToe {
 
     static Fenster f;
     static boolean DEBUG = true;
+    static Thread spiel = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            try {
+                //f = null;
+                f = new Fenster();
+                boolean nochEinSpiel = true;
+                while (nochEinSpiel) {
 
-    public static void main(String[] args) throws IOException {
+                    nochEinSpiel = new Spiel(f).spielen();
+
+                    // aufräumen
+                    //f.setVisible(false);
+                    Gewinner.setSpieler(null);
+                    Zug.setZugNummer(0);
+                    f.getSpielfeld().reset();
+
+                }
+            } catch (InterruptedException ie) {
+                System.err.println("Da ist was schief gelaufen!");
+            }
+        }
+    });
+
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         // Laden der Spielstände
         Speicher.spielstaendeEinlesen();
 
         // Spiel starten
-        try {
-            boolean nochEinSpiel = true;
-            while (nochEinSpiel) {
-
-                f = null;
-                f = new Fenster();
-                nochEinSpiel = new Spiel(f).spielen();
-
-                // aufräumen
-                f.setVisible(false);
-                Gewinner.setSpieler(null);
-                Zug.setZugNummer(0);
-
-            }
-        } catch (InterruptedException ie) {
-            System.err.println("Da ist was schief gelaufen!");
-        }
+        spiel.start();
+        spiel.join();
         Speicher.spielstaendeSpeichern();
         System.exit(0); // sofortiges Programmende
 
